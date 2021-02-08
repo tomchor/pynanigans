4c7eca53fc71cf62563c386f0b4203fdf4feaef8
tomchor <tomaschor@gmail.com>
2021-02-08 09:42:55 -0700

    added ffmpeg to py env install function

--- pyenv_install.bash
+++ pyenv_install.bash
@@ -1,4 +1,4 @@
 conda install -c conda-forge colorcet ipython matplotlib pandas scipy hvplot \
-    xgcm cmocean xarray dask netCDF4 bottleneck cartopy
+    xgcm cmocean xarray dask netCDF4 bottleneck cartopy ffmpeg
 
 pip install xmovie
