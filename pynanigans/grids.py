0d13fa5fb6fb3089f3c44c1b839523df780decc9
tomaschor <tomaschor@gmail.com>
2025-05-14 18:40:38 -0300

    remove get_distances

--- pynanigans/grids.py
+++ pynanigans/grids.py
@@ -1,57 +1,35 @@
 
-def get_coords(ds, topology="PPN",):
+def get_coords(topology="PPN"):
     """ 
-    Constructs the coords dict for ds to be passed to xgcm.Grid
+    Constructs the coords dict to be passed to xgcm.Grid
     Flat dimensions (F) are treated the same as Periodic ones (P)
     """
-    per = dict(left='x_faa', center='x_caa')
-    nper = dict(outer='x_faa', center='x_caa')
-    per = { dim : dict(left=f"{dim}F", center=f"{dim}C") for dim in "xyz" }
-    nper = { dim : dict(outer=f"{dim}F", center=f"{dim}C") for dim in "xyz" }
+    x_per = dict(left='x_faa', center='x_caa')
+    y_per = dict(left='y_afa', center='y_aca')
+    z_per = dict(left='z_aaf', center='z_aac')
+
+    x_nper = dict(outer='x_faa', center='x_caa')
+    y_nper = dict(outer='y_afa', center='y_aca')
+    z_nper = dict(outer='z_aaf', center='z_aac')
+
+    per  = dict(x = x_per,  y = y_per,  z = z_per)
+    nper = dict(x = x_nper, y = y_nper, z = z_nper)
+
     coords = { dim : per[dim] if top in "FP" else nper[dim] for dim, top in zip("xyz", topology) }
-    
+
     return coords
-
-
-def get_distances(ds, dim="x", topology="P"):
-    """
-    Get distance metrics for Center and Face points of one specific dimension ξ.
-    If the topology of this dimension is periodic, len(ξC)==len(ξF), but if it
-    is nonperiodic, then len(ξC)+1==len(ξF).
-
-    Currently does not deal with stretched domains where ΔξC!=ΔξF in the interior.
-    """
-    import numpy as np
-    import xarray as xr
-
-    Δξ_mean = ds[dim+"C"].diff(dim+"C").mean().item()
-    ΔξC = xr.DataArray(np.ones(len(ds[dim+"C"])), dims=[dim+'C'])
-    if topology=="P" or topology=="F":
-        ΔξF = xr.DataArray(np.ones(len(ds[dim+"F"])), dims=[dim+'F'])
-    elif topology=="N":
-        if len(ds[dim+"F"]) != 1:
-            interior = np.ones(len(ds[dim+"F"])-2)
-            ΔξF = xr.DataArray(np.hstack([0.5, interior, 0.5]), dims=[dim+'F'])
-        else: # Especial case of a slice in a non-periodic dimension
-            ΔξF = xr.DataArray([1], dims=[dim+'F'])
-    return Δξ_mean * xr.Dataset({f"Δ{dim}C" : ΔξC, f"Δ{dim}F" : ΔξF})
 
 
 def get_metrics(ds, topology="PPN"):
     """ 
-    Constructs the metric dict for ds.
+    Constructs the metric dict for `ds`.
     (Not sure if the metrics are correct at the boundary points
     """
 
-    for ξ, top in zip('xyz', topology):
-        ξdist = get_distances(ds, dim=ξ, topology=top)
-        ds.coords[f"Δ{ξ}C"] = ξdist[f"Δ{ξ}C"]
-        ds.coords[f"Δ{ξ}F"] = ξdist[f"Δ{ξ}F"]
-
     metrics = {
-        ('x',): ['ΔxC', 'ΔxF'], # X distances
-        ('y',): ['ΔyC', 'ΔyF'], # Y distances
-        ('z',): ['ΔzC', 'ΔzF'], # Z distances
+        ('x',): ['Δx_caa', 'Δx_faa'], # X distances
+        ('y',): ['Δy_aca', 'Δy_afa'], # Y distances
+        ('z',): ['Δz_aac', 'Δz_aaf'], # Z distances
     }
 
     return metrics
@@ -62,7 +40,7 @@ def get_grid(ds, coords=None, metrics=None, topology="PPN", **kwargs):
     import xgcm as xg
 
     if coords is None:
-        coords = get_coords(ds, topology=topology)
+        coords = get_coords(topology)
     if metrics is None:
         metrics = get_metrics(ds, topology=topology)
 
