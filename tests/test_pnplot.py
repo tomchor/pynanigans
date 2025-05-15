ec60c9b4db86993ad9bc815b9725aaadc4f435c2
tomaschor <tomaschor@gmail.com>
2025-05-15 10:31:04 -0300

    change notation in tests

--- tests/test_pnplot.py
+++ tests/test_pnplot.py
@@ -6,10 +6,10 @@ from pynanigans.pnplot import pnplot, _imshow, _pcolormesh, _contour, _contourf
 def test_pnplot():
     # Create a test dataset
     data = np.random.rand(10, 10)
-    dims = ['xC', 'yC']
+    dims = ['x_caa', 'y_aca']
     coords = {
-        'xC': np.linspace(0, 1, 10),
-        'yC': np.linspace(0, 1, 10)
+        'x_caa': np.linspace(0, 1, 10),
+        'y_aca': np.linspace(0, 1, 10)
     }
     ds = xr.Dataset(
         data_vars={'u': (dims, data)},
@@ -17,16 +17,16 @@ def test_pnplot():
     )
 
     # Test plotting
-    plot = pnplot(ds.u, x='x', y='y')
+    plot = pnplot(ds.u, x='x_caa', y='y_aca')
     assert plot is not None
 
 def test_imshow():
     # Create a test dataset
     data = np.random.rand(10, 10)
-    dims = ['xC', 'yC']
+    dims = ['x_caa', 'y_aca']
     coords = {
-        'xC': np.linspace(0, 1, 10),
-        'yC': np.linspace(0, 1, 10)
+        'x_caa': np.linspace(0, 1, 10),
+        'y_aca': np.linspace(0, 1, 10)
     }
     ds = xr.Dataset(
         data_vars={'u': (dims, data)},
@@ -34,16 +34,16 @@ def test_imshow():
     )
 
     # Test imshow
-    plot = _imshow(ds.u, x='x', y='y')
+    plot = _imshow(ds.u, x='x_caa', y='y_aca')
     assert plot is not None
 
 def test_pcolormesh():
     # Create a test dataset
     data = np.random.rand(10, 10)
-    dims = ['xC', 'yC']
+    dims = ['x_caa', 'y_aca']
     coords = {
-        'xC': np.linspace(0, 1, 10),
-        'yC': np.linspace(0, 1, 10)
+        'x_caa': np.linspace(0, 1, 10),
+        'y_aca': np.linspace(0, 1, 10)
     }
     ds = xr.Dataset(
         data_vars={'u': (dims, data)},
@@ -51,16 +51,16 @@ def test_pcolormesh():
     )
 
     # Test pcolormesh
-    plot = _pcolormesh(ds.u, x='x', y='y')
+    plot = _pcolormesh(ds.u, x='x_caa', y='y_aca')
     assert plot is not None
 
 def test_contour():
     # Create a test dataset
     data = np.random.rand(10, 10)
-    dims = ['xC', 'yC']
+    dims = ['x_caa', 'y_aca']
     coords = {
-        'xC': np.linspace(0, 1, 10),
-        'yC': np.linspace(0, 1, 10)
+        'x_caa': np.linspace(0, 1, 10),
+        'y_aca': np.linspace(0, 1, 10)
     }
     ds = xr.Dataset(
         data_vars={'u': (dims, data)},
@@ -68,16 +68,16 @@ def test_contour():
     )
 
     # Test contour
-    plot = _contour(ds.u, x='x', y='y')
+    plot = _contour(ds.u, x='x_caa', y='y_aca')
     assert plot is not None
 
 def test_contourf():
     # Create a test dataset
     data = np.random.rand(10, 10)
-    dims = ['xC', 'yC']
+    dims = ['x_caa', 'y_aca']
     coords = {
-        'xC': np.linspace(0, 1, 10),
-        'yC': np.linspace(0, 1, 10)
+        'x_caa': np.linspace(0, 1, 10),
+        'y_aca': np.linspace(0, 1, 10)
     }
     ds = xr.Dataset(
         data_vars={'u': (dims, data)},
@@ -85,5 +85,5 @@ def test_contourf():
     )
 
     # Test contourf
-    plot = _contourf(ds.u, x='x', y='y')
+    plot = _contourf(ds.u, x='x_caa', y='y_aca')
     assert plot is not None
