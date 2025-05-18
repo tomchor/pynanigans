58888014ff57a0da048bf7156a794ec9adb7f9be
tomaschor <tomaschor@gmail.com>
2025-05-18 10:50:46 -0300

    shorter netcdf file

--- tests/make_netcdf.jl
+++ tests/make_netcdf.jl
@@ -1,6 +1,7 @@
 using Oceananigans
+using NCDatasets
 grid = RectilinearGrid(size=(4, 4, 4), extent=(1, 1, 1));
 model = NonhydrostaticModel(; grid)
-simulation = Simulation(model; Δt = 1, stop_time=10)
+simulation = Simulation(model; Δt=1, stop_time=2)
 simulation.output_writers[:nc] = NetCDFWriter(model, model.velocities, schedule=IterationInterval(1), filename="uvw.nc", overwrite_existing=true)
 run!(simulation)
