21113793b360022dd92166416d636b0a47d90205
Tomás Chor <tomaschor@gmail.com>
2025-05-18 07:20:45 -0700

    Test with an actual NetCDF file (#8)
    * create pyproject.toml
    
    * add tomli to dependencies
    
    * install tomli on github actions
    
    * quick script to generate a netcdf file in Oceananigans
    
    * create test to read netcdf
    
    * better grid tests
    
    * shorter netcdf file
    
    * include julia's Project.toml
    
    * install netcdf dependencies
    
    * test something differnet
    
    * get_metrics is a little better
    
    * install netcdf4 again
    
    * fix one test
    
    * add decode_times
    
    * fix test

---
+++ tests/make_netcdf.jl
@@ -0,0 +1,7 @@
+using Oceananigans
+using NCDatasets
+grid = RectilinearGrid(size=(4, 4, 4), extent=(1, 1, 1));
+model = NonhydrostaticModel(; grid)
+simulation = Simulation(model; Δt=1, stop_time=2)
+simulation.output_writers[:nc] = NetCDFWriter(model, model.velocities, schedule=IterationInterval(1), filename="uvw.nc", overwrite_existing=true)
+run!(simulation)
--- tests/test_grids.py
+++ tests/test_grids.py
@@ -1,7 +1,9 @@
 import pytest
 import xarray as xr
 import numpy as np
+from pynanigans.utils import open_simulation
 from pynanigans.grids import get_coords, get_metrics, get_grid
+import os
 
 def test_get_coords():
     # Test periodic coordinates
@@ -32,9 +34,67 @@ def test_get_metrics():
     )
     
     # Test metrics
-    metrics = get_metrics(ds)
+    metrics = get_metrics(get_coords("PPP"))
     assert ('x',) in metrics
     assert ('y',) in metrics
     assert ('z',) in metrics
     assert "Δx_caa" in metrics[('x',)]
     assert "Δx_faa" in metrics[('x',)]
+
+def test_read_uvw_nc():
+    # Get the path to the test data file
+    test_data_path = os.path.join(os.path.dirname(__file__), "uvw.nc")
+
+    # Read the netCDF file
+    grid, ds = open_simulation(test_data_path, topology="PPN", decode_times=False)
+
+    # Basic dataset structure checks
+    assert "u" in ds
+    assert "v" in ds
+    assert "w" in ds
+
+    assert "Δx_caa" in ds
+    assert "Δx_faa" in ds
+    assert "Δy_aca" in ds
+    assert "Δy_afa" in ds
+    assert "Δz_aac" in ds
+    assert "Δz_aaf" in ds
+
+    # Check dimensions
+    assert "time" in ds.dims
+    assert "x_caa" in ds.dims
+    assert "x_faa" in ds.dims
+    assert "y_aca" in ds.dims
+    assert "y_afa" in ds.dims
+    assert "z_aac" in ds.dims
+    assert "z_aaf" in ds.dims
+
+    # Check coordinate variables
+    assert "time" in ds.coords
+    assert "x_caa" in ds.coords
+    assert "x_faa" in ds.coords
+    assert "y_aca" in ds.coords
+    assert "y_afa" in ds.coords
+    assert "z_aac" in ds.coords
+    assert "z_aaf" in ds.coords
+
+    # Check data types and shapes
+    assert ds.u.dtype in [np.float32, np.float64]
+    assert ds.v.dtype in [np.float32, np.float64]
+    assert ds.w.dtype in [np.float32, np.float64]
+
+    # Check that sizes are right after operations
+    assert grid.diff(ds.u, "x").shape[1] == len(ds.x_faa)
+    assert grid.diff(ds.v, "y").shape[2] == len(ds.y_afa)
+    assert grid.diff(ds.w, "z").shape[3] == len(ds.z_aac)
+
+    assert grid.interp(ds.u, "x").shape[1] == len(ds.x_faa)
+    assert grid.interp(ds.v, "y").shape[2] == len(ds.y_afa)
+    assert grid.interp(ds.w, "z").shape[3] == len(ds.z_aac)
+
+    assert "z_aaf" in grid.interp(ds.u, "z").coords
+    assert "z_aaf" in grid.interp(ds.v, "z").coords
+    assert "x_faa" in grid.interp(ds.w, "x").coords
+
+    # Close the dataset
+    ds.close()
